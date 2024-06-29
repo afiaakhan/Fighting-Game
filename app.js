3fc5ae958e18189d1f8d7f4d8293439e1dc6bdea
Aliza Khan <alizaadi2@gmail.com>
2024-06-29 21:06:48 +0500

    edited js

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
+let left = 0
+
 function startGame() {
     var start = document.getElementById('start');
     start.style.display = 'none'
-    var left = 0
+   
     function game() {
         console.log(left)
         console.log(event.keyCode);
-        var character = document.getElementById("character")
-        if (event.keyCode === 39 && left < 1600) {   //forward-right key
+        var character = document.getElementById('character');
+        if (event.keyCode === 39 && left< 930) {   //forward-right key
             left = left + 10;
             character.style.left = left + 'px'
             character.src = "Animations/forward-2.gif"
         }
 
-        if (event.keyCode === 37 && left > -70) {  //left key
+        if (event.keyCode === 37 && left>37) {  //left key
             left = left - 10;
             character.style.left = left + 'px'
             character.src = "Animations/forward-2.gif"
         }
 
-        if (event.keyCode === 32 && left < 1600) {  //spacebar  
-            // character.style.height = 350 + 'px'
-            // character.style.width = 350 + 'px'
+        if (event.keyCode === 32) {  //spacebar  
             character.src = "Animations/shoot-2.gif"
         }
 
@@ -28,22 +28,22 @@ function startGame() {
             character.src = "Animations/sit-2.gif"
         }
 
-        if (event.keyCode === 38 && left < 1600) {      //up key
-            character.style.left = left + 'px'
+        if (event.keyCode === 38) {      //up key
+            // character.style.left = left + 'px'
             character.src = "Animations/jump-2.gif"
         }
 
-        if (event.keyCode === 70 && left < 1600) {      //k key to kick
+        if (event.keyCode === 70) {      //k key to kick
             character.src = "Animations/flip-2.gif"
         }
 
-        if (event.keyCode === 72 && left < 1600) {  // h key to hide
+        if (event.keyCode === 72) {  // h key to hide
             character.style.display = "block"
             character.style.top = 0 + 'px'
             character.src = "Animations/hide-2.gif"
             setTimeout(function () {
                 character.src = "Animations/forward-2.gif"
-                character.style.top = 340 + 'px'
+                // character.style.top = 340 + 'px'
             }, 10000)
         }
     }
